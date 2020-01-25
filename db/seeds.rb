fe25a7806175f3206adcddb402fbf9ecc0a80003
Charlotte Waller <charlotteawaller@gmail.com>
2020-01-25 15:06:40 +0000

    Getting doses controller to save

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "json"
+require "open-uri"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# puts 'Cleaning ingredients database...'
+# Ingredient.destroy_all
+# puts 'Creating ingredients... '
+
+# ingredient_type = []
+# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# ingredients_serialized = open(url).read
+# ingredients = JSON.parse(ingredients_serialized)
+# ingredients["drinks"].each do |ingredient|
+#   ingredient_type << { name: ingredient['strIngredient1'] }
+# end
+# ingredient.create!(ingredient_type)
+
+puts 'Cleaning ingredients database...'
+Ingredient.destroy_all
+puts 'Creating ingredients... '
+ingredient_attributes = []
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)
+ingredients["drinks"].each do |ingredient|
+  ingredient_attributes << { name: ingredient["strIngredient1"] }
+end
+Ingredient.create!(ingredient_attributes)
+
