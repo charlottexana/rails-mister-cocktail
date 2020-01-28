5bc5d6f32dc64b22edecd3936247957bd4004c95
Charlotte Waller <charlotteawaller@gmail.com>
2020-01-28 15:42:46 +0000

    Used solution to get it working :(

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,14 +1,8 @@
 class DosesController < ApplicationController
-
-  def index
-    @doses = Dose.all
-  end
-
-  def new
-    # @dose = Dose.new(dose_params)
-    @cocktail = Cocktail.find(params[:cocktail_id])
-    @dose = Dose.new
-  end
+  # def new
+  #   @cocktail = Cocktail.find(params[:cocktail_id])
+  #   @dose = Dose.new
+  # end
 
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
@@ -17,18 +11,15 @@ class DosesController < ApplicationController
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-      render 'cocktails/new'
+      @review = Review.new
+      render "cocktails/show"
     end
   end
 
   def destroy
-    # @dose = Dose.find(params[:cocktail_id])
-    # @dose.destroy
-    # redirect_to cocktail_path(@dose.cocktail)
     @dose = Dose.find(params[:id])
-    @cocktail = @dose.cocktail
     @dose.destroy
-    redirect_to cocktail_path(@cocktail)
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
@@ -36,5 +27,4 @@ class DosesController < ApplicationController
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
+# Rails.application.routes.draw do
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#   # cocktails routes
+#   resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
+#     resources :doses
+#   end
+#   resources :doses, only: [:destroy, :new, :create, :show]
+
+# end
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # cocktails routes
-  resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
-    resources :doses
+  root to: "cocktails#index"
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:create]
   end
-  resources :doses, only: [:destroy, :new, :create]
-
+  resources :doses, only: [:destroy]
 end
