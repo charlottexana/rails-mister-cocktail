415f2ae8353c4cc35706339046f52cb625fbf024
Charlotte Waller <charlotteawaller@gmail.com>
2020-01-30 19:44:46 +0000

    Did seed manually as API was broken on Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,32 +3,27 @@ require "open-uri"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+# Keep this!!!!!!!!!!!!!!!
+
 # puts 'Cleaning ingredients database...'
 # Ingredient.destroy_all
 # puts 'Creating ingredients... '
-
-# ingredient_type = []
+# ingredient_attributes = []
 # url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 # ingredients_serialized = open(url).read
 # ingredients = JSON.parse(ingredients_serialized)
 # ingredients["drinks"].each do |ingredient|
-#   ingredient_type << { name: ingredient['strIngredient1'] }
+#   ingredient_attributes << { name: ingredient["strIngredient1"] }
 # end
-# ingredient.create!(ingredient_type)
+# Ingredient.create!(ingredient_attributes)
 
-puts 'Cleaning ingredients database...'
-Ingredient.destroy_all
-puts 'Creating ingredients... '
-ingredient_attributes = []
-url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-ingredients_serialized = open(url).read
-ingredients = JSON.parse(ingredients_serialized)
-ingredients["drinks"].each do |ingredient|
-  ingredient_attributes << { name: ingredient["strIngredient1"] }
-end
-Ingredient.create!(ingredient_attributes)
-
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "tequila")
+Ingredient.create(name: "bourbon")
+Ingredient.create(name: "whiskey")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "baileys")
